1902528c4a570b0fdca8c046815e635521aa615c
jeanlego <jeanphilippe.legault@unb.ca>
2018-05-22 20:20:13 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,7 +1,15 @@
 #!/bin/bash
-apt-get update
-apt-get upgrade -y
-apt-get install \
+apt-get update && apt-get upgrade -y
+
+#set hostname
+HOST_NM=$(cat /sys/class/net/eth0/address  | tr -d :)
+sed -i 's/pine64so/$HOST_NM/g' /etc/hosts
+sed -i 's/pine64so/$HOST_NM/g' /etc/hostname
+hostname $HOST_NM
+systemctl hostname restart
+
+#install docker
+apt-get install --fix-missing \
      apt-transport-https \
      ca-certificates \
      curl \
@@ -10,11 +18,13 @@ apt-get install \
 curl -fsSLk https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
+apt-get update --no-check-certificate
 apt-get install -y docker-ce
 
+#install docker compose
 curl -Lk https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
 groupadd docker
+
 usermod -aG docker root
 usermod -aG docker admin
