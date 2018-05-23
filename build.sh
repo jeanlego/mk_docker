2f4c1ea6a79c6370b57c4178dfe4239716add986
jeanlego <jeanphilippe.legault@unb.ca>
2018-05-23 12:56:48 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,27 +1,13 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y
-apt-get install --fix-missing -y ca-certificates curl gnupg2 ntpdate software-properties-common ntpdate apt-transport-https hostname
 
 #synchronize time
+apt-get update
+apt-get install --fix-missing ntpdate
 ntpdate -u pool.ntp.org && hwclock -w
 
-#set hostname
-HOST_NM=$(cat /sys/class/net/eth0/address  | tr -d :)
-hostname $HOST_NM
-printf "\
-  127.0.0.1   localhost $HOST_NM \n \
-  ::1         localhost $HOST_NM ip6-localhost ip6-loopback \n \
-  fe00::0     ip6-localnet \n \
-  ff00::0     ip6-mcastprefix \n \
-  ff02::1     ip6-allnodes \n \
-  ff02::2     ip6-allrouters \n \
-  " > /etc/hosts
-
-printf "\
-  $HOST_NM \n \
-  " > /etc/hostname
-
 #install docker
+apt-get update
+apt-get install --fix-missing -y ca-certificates curl gnupg2 software-properties-common apt-transport-https hostname
 curl -fsSLk https://download.docker.com/linux/ubuntu/gpg | apt-key add -
 apt-key fingerprint 0EBFCD88
 add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
@@ -31,8 +17,34 @@ apt-get install -y docker-ce
 #install docker compose
 curl -Lk https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
 chmod +x /usr/local/bin/docker-compose
-groupadd docker
 
+#set permissions
+groupadd docker
 usermod -aG docker root
 usermod -aG docker admin
+
+#set hostname
+HOST_NM=$(cat /sys/class/net/eth0/address  | tr -d :)
+hostname $HOST_NM
+printf "\
+  127.0.0.1   localhost $HOST_NM \n \
+  " > /etc/hosts
+
+printf "\
+  $HOST_NM \n \
+  " > /etc/hostname
+  
+#set inet  
+printf "\
+    auto lo eth0 \n \
+    allow-hotplug eth0 \n \
+    iface eth0 inet dhcp \n \
+    " > /etc/network/interfaces
+printf "\
+    nameserver 8.8.8.8 \n \
+    nameserver 8.8.4.4 \n \
+    " > /etc/resolv.conf
+
+#disable networkmanager
+systemctl disable NetworkManager.service
 reboot
