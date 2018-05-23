f6ef51d64f2ac127158273ab889913c499576e46
jeanlego <jeanphilippe.legault@unb.ca>
2018-05-22 21:40:29 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,28 +1,28 @@
 #!/bin/bash
-apt-get update && apt-get upgrade -y
-apt-get install --fix-missing -y ca-certificates curl gnupg2 ntpdate software-properties-common ntpdate apt-transport-https hostname
+# apt-get update && apt-get upgrade -y
+# apt-get install --fix-missing -y ca-certificates curl gnupg2 ntpdate software-properties-common ntpdate apt-transport-https hostname
 
-#synchronize time
-ntpdate -u pool.ntp.org && hwclock -w
+# #synchronize time
+# ntpdate -u pool.ntp.org && hwclock -w
 
 #set hostname
 HOST_NM=$(cat /sys/class/net/eth0/address  | tr -d :)
-sed -i 's/pine64so/${HOST_NM}/g' /etc/hosts
-sed -i 's/pine64so/${HOST_NM}/g' /etc/hostname
+sed -i 's/HOST_NM/${HOST_NM}/g' /etc/hosts
+sed -i 's/HOST_NM/${HOST_NM}/g' /etc/hostname
 hostname ${HOST_NM}
 systemctl hostname restart
 
-#install docker
-curl -fsSLk https://download.docker.com/linux/ubuntu/gpg | apt-key add -
-apt-key fingerprint 0EBFCD88
-add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
-apt-get update
-apt-get install -y docker-ce
+# #install docker
+# curl -fsSLk https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+# apt-key fingerprint 0EBFCD88
+# add-apt-repository "deb [arch=arm64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
+# apt-get update
+# apt-get install -y docker-ce
 
-#install docker compose
-curl -Lk https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
-chmod +x /usr/local/bin/docker-compose
-groupadd docker
+# #install docker compose
+# curl -Lk https://github.com/docker/compose/releases/download/1.21.2/docker-compose-$(uname -s)-$(uname -m) -o /usr/local/bin/docker-compose
+# chmod +x /usr/local/bin/docker-compose
+# groupadd docker
 
-usermod -aG docker root
-usermod -aG docker admin
+# usermod -aG docker root
+# usermod -aG docker admin
